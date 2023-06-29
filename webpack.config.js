7faf1ef196926bcad5265ed4ad15e7a8eaca1349
Valentin Himself <valentinhimself@gmail.com>
2023-06-29 13:59:50 -0700

    Add copy plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -40,6 +41,14 @@ module.exports = {
       title: 'Webpack App',
       filename: 'index.html',
       template: 'src/template.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'src/assets/', // Source directory of SVG files
+          to: 'assets/', // Destination directory in the output `dist` folder
+        },
+      ],
     }),
   ],
 };
